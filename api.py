66e72d10ca748c799abb8728dc0676c98505f2b1
victorsavaniniype <victor.savanini@ype.ind.br>
2023-11-07 11:06:54 -0300

    arquivo semi final

--- api.py
+++ api.py
@@ -1,1 +1,13 @@
-# Agora já da para começar
+from mp_api.client import MPRester
+
+key = "qx3M9lC8cMaGU3gp0ZpKSPor69SxkCU9"
+
+with MPRester(key) as mpr:
+    dos = mpr.get_dos_by_material_id("mp-149")
+    fp = dos.get_dos_fp()
+
+    # print(dos.get_normalized())
+    # print(dos.get_site_dos())
+    # print(dos.get_densities())
+
+    print(help(dos))
